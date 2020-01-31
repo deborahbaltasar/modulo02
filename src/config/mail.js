d39eba59e42e53830784fa5933e9b8e7eec403c3
Deborah <deborahbnogueira.dn@gmail.com>
2020-01-31 13:33:14 -0300

    commit teste

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1342c4d197be1b',
+    pass: '17deeae8548e0a'
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
